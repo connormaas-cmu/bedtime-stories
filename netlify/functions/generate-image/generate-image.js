d1b347577505dbe832df81b728de666d5d674bdf
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-19 23:00:18 -0400

    fixing import syntax

--- netlify/functions/generate-image/generate-image.js
+++ netlify/functions/generate-image/generate-image.js
@@ -1,4 +1,7 @@
-const fetch = require('node-fetch');
+let fetch;
+import('node-fetch').then(module => {
+  fetch = module.default;
+});
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
