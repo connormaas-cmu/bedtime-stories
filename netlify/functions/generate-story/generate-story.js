cc0b4f2fd509cb5eaab3a783cf8fe65f49195cb8
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-20 14:16:32 -0400

    test

--- netlify/functions/generate-story/generate-story.js
+++ netlify/functions/generate-story/generate-story.js
@@ -1,13 +1,18 @@
 async function openaiSetup() {
     const module = await import('openai')
+    throw new Error(module)
     const key = process.env.TEXT_API_KEY
-    throw new Error(key)
     return new module({ apiKey: key });
 }
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-    const openai = openaiSetup()
+    try {
+        const openai = openaiSetup()
+    } catch (error) {
+        throw new Error(error)
+    }
+
 
     try {
         const { prompt } = JSON.parse(event.body);
