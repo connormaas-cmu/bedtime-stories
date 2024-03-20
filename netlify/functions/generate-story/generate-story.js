ded7c89b763077c3c1ee96caf5fff6a84383b238
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-20 13:52:09 -0400

    updating openai

--- netlify/functions/generate-story/generate-story.js
+++ netlify/functions/generate-story/generate-story.js
@@ -3,27 +3,25 @@ async function setupFetch() {
   return module.default;
 }
 
+async function openaiSetup() {
+    const module = await import('openai')
+    return new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+}
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   const fetch = await setupFetch();
+  const openai = openaiSetup()
+
   try {
       const { prompt } = JSON.parse(event.body);
       const API_KEY = process.env.TEXT_API_KEY;
-      const response = await fetch('https://api.openai.com/v1/chat/completions', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${API_KEY}`,
-          },
-          body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              messages: [{"role": "user", "content": prompt}]
-          }),
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
       });
-
-      const data = await response.json();
-
-      const story = data.choices[0].message.content;
+  
+      const story = response.data.choices[0].message.content;
 
       return {
           statusCode: 200,
