57b490ff4411d1d60a7a69b8f093fdbd7282e77e
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-19 23:31:17 -0400

    testing 3

--- netlify/functions/generate-image/generate-image.js
+++ netlify/functions/generate-image/generate-image.js
@@ -7,8 +7,8 @@ async function setupFetch() {
 const handler = async (event) => { 
   const fetch = await setupFetch();
   try {
-    throw new Error(event.body)
     const { text } = JSON.parse(event.body);  
+    throw new Error(event.body)
     const API_KEY = '3ec93807f6msh31ed4d7d260d5d3p175ee1jsn3ce1ecb6e9a2';
     const API_HOST = 'omniinfer.p.rapidapi.com';
 
