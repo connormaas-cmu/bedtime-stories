5c4163f97656f40033f2ff4003e82c6d4398107a
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-21 01:01:49 -0400

    try again

--- netlify/functions/continue-generation/continue-generation.js
+++ netlify/functions/continue-generation/continue-generation.js
@@ -10,7 +10,7 @@ const handler = async (event) => {
       const body = JSON.parse(event.body);  
       const prom = body.prompt;
       const text = body.text;
-      const prompt = "We are creating a subtly educational story in the form of a children's tale about the following:\n\n'" + prom + "'\n\nHere is the story so far:\n\n" + text + "\n\nPlease respond with about 75 words that add to the end story (continue from where it left off), but leave room for more story to be told. Make it as educational ass possible. Do not start your sentence with 'as.'"
+      const prompt = "We are creating a subtly educational story in the form of a children's tale about the following:\n\n'" + prom + "'\n\nHere is the story so far:\n\n" + text + "\n\nPlease respond with about 75 words that add to the end story (continue from where it left off), but leave room for more story to be told. Make it as educational ass possible. Do not start your sentence with 'as.' or repeat previous transitions."
 
       const API_KEY = process.env.API_KEY;
       const API_HOST = 'open-ai21.p.rapidapi.com';
