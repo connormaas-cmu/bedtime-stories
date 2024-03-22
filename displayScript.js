f48614ab4647b83195b94e3542fbe95b5780bbc5
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-03-22 06:22:23 -0400

    dialogue update

--- displayScript.js
+++ displayScript.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', async function() {
                 storyElement6.textContent = newData.result + " The end!"
                 
                 setTimeout(() => {
-                    const newRawText = newData.result + ", Theeeeeeeee, ennnnnnd"
+                    const newRawText = newData.result + ", Thee, end"
                     const newModText = newRawText.replace(/[.!?-]/g, ",");
                     fetch('/.netlify/functions/generate-audio', {
                         method: 'POST',
